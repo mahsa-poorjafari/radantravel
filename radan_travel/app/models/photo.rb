9d64ca1db1f355004b88fd579ab9da0372be27ea
mahsa <m_mw7462@yahoo.com>
2014-11-09 13:16:11 +0330

    add tour-package to multi slider

--- radan_travel/app/models/photo.rb
+++ radan_travel/app/models/photo.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 class Photo < ActiveRecord::Base
-  has_attached_file :image, :styles => { :original => "700x650>" , :medium => "450x450>", :small => "300x350>" }
+  has_attached_file :image, :styles => { :original => "700x650>" , :medium => "450x450>", :small => "300x350>", :toursize => "248x370#" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
   belongs_to :city
   belongs_to :country
