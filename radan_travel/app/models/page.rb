f036e758b149966298edcbd724b4ef26a3a2b8e1
mahsa <m_mw7462@yahoo.com>
2014-11-09 13:16:11 +0330

    add tour-package to multi slider

--- radan_travel/app/models/page.rb
+++ radan_travel/app/models/page.rb
@@ -1,4 +1,25 @@
 class Page < ActiveRecord::Base
+  extend FriendlyId  
+  friendly_id :title_en
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  
+  def title
+    if I18n.locale == :ar
+      self.read_attribute("title_ar")
+    elsif I18n.locale == :en
+      self.read_attribute("title_en")
+    else
+      self.read_attribute("title_fa")
+    end    
+  end
+  def text
+    if I18n.locale == :ar
+      self.read_attribute("text_ar")
+    elsif I18n.locale == :en
+      self.read_attribute("text_en")
+    else
+      self.read_attribute("text_fa")
+    end    
+  end
 end
