098c90d50ad81bf5ca8b6348f9a31e8982ffb0c5
mahsa <m_mw7462@yahoo.com>
2014-11-12 11:42:22 +0330

    add tumbnails slider for tour show page, shoe tour_comment in tour show page

--- radan_travel/app/controllers/tours_controller.rb
+++ radan_travel/app/controllers/tours_controller.rb
@@ -95,6 +95,6 @@ class ToursController < ApplicationController
       :plane_id, :hotel_id, :country_id, :city_id,
       :price, :validate_date_from_fa, :validate_date_until_fa, :coming_soon, :itinerary_fa, :itinerary_en, :itinerary_ar,
       :documents_fa, :documents_en, :documents_ar, :decription_image,
-      photos_attributes: [:image, :description_fa, :description_en, :description_ar, :tour_id, :_destroy, :_update] )
+      photos_attributes: [:id, :image, :description_fa, :description_en, :description_ar, :tour_id, :_destroy, :_update] )
     end
 end
