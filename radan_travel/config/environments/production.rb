2bbd61c772cf4948519db3cff70174f11e155632
mahsa <m_mw7462@yahoo.com>
2014-12-27 11:07:45 +0330

    try to load js, add config.assets.precompile += %w( application-*.js ) in production.rb

--- radan_travel/config/environments/production.rb
+++ radan_travel/config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
   config.assets.precompile = ['*.js', '*.css', '*.css.scss']
-
+  config.assets.precompile += %w( application-*.js )
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
