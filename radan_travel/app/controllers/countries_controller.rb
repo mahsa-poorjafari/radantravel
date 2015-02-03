3c18e01c4d09021ef587a6bd7e0bb2170c5e554a
mahsa <m_mw7462@yahoo.com>
2015-02-03 19:10:41 +0330

    fix bug in tours-group submenu

--- radan_travel/app/controllers/countries_controller.rb
+++ radan_travel/app/controllers/countries_controller.rb
@@ -13,9 +13,9 @@ class CountriesController < ApplicationController
   def show
   end
   def show_fun_tours_for_country
-    @country = Country.find(params[:country_id])
+    @country = Country.find(params[:country_id])    
+    @tour_group = ContinentCategory.find(params[:continent_id])
     
-    @fun_group = ContinentCategory.find_by_title_fa('تورهای تفریحی')
     
   end
 
