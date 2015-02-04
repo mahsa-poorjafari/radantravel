3ebe4855daeca3fdbfde21e5a419779192acd831
mahsa <m_mw7462@yahoo.com>
2015-02-04 18:29:26 +0330

    change design of continent submenu

--- radan_travel/app/models/visa_form.rb
+++ radan_travel/app/models/visa_form.rb
@@ -2,4 +2,5 @@
 class VisaForm < ActiveRecord::Base
   has_many :passengers
   accepts_nested_attributes_for :passengers, reject_if: :all_blank, allow_destroy: true
+  validates :I_agree, :presence => {:message => 'Please Check The Visa process agreement'}
 end
